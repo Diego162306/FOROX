2f17af4b80ae03cf04ef53bdb23e45805f454e5d
carlos_david <dabid1053e.gt@gmail.com>
2025-05-29 20:44:39 -0500

    CategoriaService  mejora v1.1

--- foro-ing/src/main/java/fori/ing/com/base/controlller/services/CategoriaService.java
+++ foro-ing/src/main/java/fori/ing/com/base/controlller/services/CategoriaService.java
@@ -5,13 +5,10 @@ import java.util.List;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.hilla.BrowserCallable;
-import com.vaadin.hilla.mappedtypes.Pageable;
 
 import fori.ing.com.base.controlller.dao.dao_models.DaoCategoria;
 import fori.ing.com.base.models.Categoria;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 @BrowserCallable
 @AnonymousAllowed
@@ -29,17 +26,13 @@ public class CategoriaService {
         }
     }
 
-    public void update(Integer id, @NotEmpty @NotBlank @NotNull String nombre, @NotEmpty String descripcion) throws Exception{
+    public void update(Integer id, @NotEmpty String nombre, @NotEmpty String descripcion) throws Exception{
         if(id != null && id > 0 && nombre.trim().length() > 0 && descripcion.trim().length() > 0);
         db.getObj().setNombre(nombre);
         db.getObj().setDescripcion(descripcion);
         if(!db.update(id-1)){
             throw new Exception("No se puede guardar los datos de artista");
         }
-    }
-
-    public List<Categoria> list(Pageable pageable){
-        return Arrays.asList(db.listAll().toArray());
     }
 
     public List<Categoria>listAllCategoria(){
