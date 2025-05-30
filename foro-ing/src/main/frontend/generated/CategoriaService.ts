23567d8b1eb710b4295ebc901414b5f1b123ae5b
Juan Diego Veintimilla Riofrio <juan.veintimilla@unl.edu.ec>
2025-05-30 11:41:56 -0500

    Update new format

--- foro-ing/src/main/frontend/generated/CategoriaService.ts
+++ foro-ing/src/main/frontend/generated/CategoriaService.ts
@@ -1,9 +1,7 @@
 import { EndpointRequestInit as EndpointRequestInit_1 } from "@vaadin/hilla-frontend";
-import type Pageable_1 from "./com/vaadin/hilla/mappedtypes/Pageable.js";
 import client_1 from "./connect-client.default.js";
 import type Categoria_1 from "./fori/ing/com/base/models/Categoria.js";
 async function createCategoria_1(nombre: string | undefined, descripcion: string | undefined, init?: EndpointRequestInit_1): Promise<void> { return client_1.call("CategoriaService", "createCategoria", { nombre, descripcion }, init); }
-async function list_1(pageable: Pageable_1 | undefined, init?: EndpointRequestInit_1): Promise<Array<Categoria_1 | undefined> | undefined> { return client_1.call("CategoriaService", "list", { pageable }, init); }
 async function listAll_1(init?: EndpointRequestInit_1): Promise<Array<Categoria_1 | undefined> | undefined> { return client_1.call("CategoriaService", "listAll", {}, init); }
-async function updateCategoria_1(id: number | undefined, nombre: string | undefined, descripcion: string | undefined, init?: EndpointRequestInit_1): Promise<void> { return client_1.call("CategoriaService", "updateCategoria", { id, nombre, descripcion }, init); }
-export { createCategoria_1 as createCategoria, list_1 as list, listAll_1 as listAll, updateCategoria_1 as updateCategoria };
+async function updateCategoria_1(id: number | undefined, nombre: string | undefined, decripcion: string | undefined, init?: EndpointRequestInit_1): Promise<void> { return client_1.call("CategoriaService", "updateCategoria", { id, nombre, decripcion }, init); }
+export { createCategoria_1 as createCategoria, listAll_1 as listAll, updateCategoria_1 as updateCategoria };
