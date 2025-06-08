a58a2390ab50b173de2034d351005bfa00ccaba9
Juan Diego Veintimilla Riofrio <juan.veintimilla@unl.edu.ec>
2025-06-08 18:15:29 -0500

    Delete Code
    eliminación de proyecto con errores

--- foro-ing/src/main/frontend/generated/fori/ing/com/base/models/Cuenta.ts
+++
@@ -1,8 +0,0 @@
-interface Cuenta {
-    id?: number;
-    correo?: string;
-    clave?: string;
-    id_usuario?: number;
-    rol?: string;
-}
-export default Cuenta;
--- foro-ing/src/main/frontend/generated/fori/ing/com/base/models/TipoArchivoModel.ts
+++
@@ -1,7 +0,0 @@
-import { _enum as _enum_1, EnumModel as EnumModel_1, makeEnumEmptyValueCreator as makeEnumEmptyValueCreator_1 } from "@vaadin/hilla-lit-form";
-import TipoArchivo_1 from "./TipoArchivo.js";
-class TipoArchivoModel extends EnumModel_1<typeof TipoArchivo_1> {
-    static override createEmptyValue = makeEnumEmptyValueCreator_1(TipoArchivoModel);
-    readonly [_enum_1] = TipoArchivo_1;
-}
-export default TipoArchivoModel;
