7105c13c64fdbef12cb1ae87948a8a6430c8e685
Roberto <roberto.lliguin@unl.edu.ec>
2025-06-10 10:38:04 -0500

    Delete proyect

--- src/main/frontend/generated/endpoints.ts
+++
@@ -1,9 +0,0 @@
-import * as ArchivoAdjuntoService_1 from "./ArchivoAdjuntoService.js";
-import * as CategoriaService_1 from "./CategoriaService.js";
-import * as CuentaService_1 from "./CuentaService.js";
-import * as PreguntaService_1 from "./PreguntaService.js";
-import * as RespuestaService_1 from "./RespuestaService.js";
-import * as TaskService_1 from "./TaskService.js";
-import * as UsuarioRespuestaService_1 from "./UsuarioRespuestaService.js";
-import * as UsuarioService_1 from "./UsuarioService.js";
-export { ArchivoAdjuntoService_1 as ArchivoAdjuntoService, CategoriaService_1 as CategoriaService, CuentaService_1 as CuentaService, PreguntaService_1 as PreguntaService, RespuestaService_1 as RespuestaService, TaskService_1 as TaskService, UsuarioRespuestaService_1 as UsuarioRespuestaService, UsuarioService_1 as UsuarioService };
